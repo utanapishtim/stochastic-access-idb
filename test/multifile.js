a6291657984ec1c844509cfc43bf96a76d4fe1b6
Yuval Kogman <nothingmuch@woobling.org>
2017-08-21 16:26:40 +0100

    Correct multifile.js for length satisfiability errors

--- test/multifile.js
+++ test/multifile.js
@@ -3,7 +3,7 @@ var random = require('../')('testing-' + Math.random(), { size: 5 })
 var bfrom = require('buffer-from')
 
 test('multiple files cool and good', function (t) {
-  t.plan(11)
+  t.plan(14)
   var cool = random('cool.txt')
   var and = random('and.txt')
   var good = random('good.txt')
@@ -24,9 +24,16 @@ test('multiple files cool and good', function (t) {
         t.equal(buf.toString(), 'RAN')
         good.write(90, bfrom('DO YOU EVER JUST... TEAPOT'), function (err) {
           t.ifError(err)
-          good.read(110, 10, function (err, buf) {
+          good.read(110, 6, function (err, buf) {
             t.ifError(err)
-            t.equal(buf.toString(), 'TEAPOT\0\0\0\0')
+            t.equal(buf.toString(), 'TEAPOT')
+            good.read(86, 10, function (err, buf) {
+              t.ifError(err)
+              t.equal(buf.toString(), '\0\0\0\0DO YOU')
+              good.read(110, 10, function (err, buf) {
+                t.ok(err, "should error when reading past end of file")
+              })
+            })
           })
         })
       })
